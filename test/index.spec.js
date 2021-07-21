820494b3d7c89e47d69791ace9fe5a078d5e30eb
majo-cerpe <mariajose_cerpe@outlook.com>
2021-07-21 09:45:14 -0400

    test ordinario

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,8 +1,8 @@
 // importamos la funcion que vamos a testear
-import { myFunction } from '../src/lib/index';
+import { displayRespectiveTemplate } from '../src/lib/router.js';
 
-describe('myFunction', () => {
-  it('debería ser una función', () => {
-    expect(typeof myFunction).toBe('function');
+describe('displayRespectiveTemplate', () => {
+  it('should be a function', () => {
+    expect(typeof displayRespectiveTemplate).toBe('function');
   });
 });
