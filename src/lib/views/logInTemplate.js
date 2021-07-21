41ccbc1ae99133f7e7b9160788239d5f271417ab
Katherine-valcarce <katherine.v@live.cl>
2021-07-21 09:24:41 -0400

    first mobile

--- src/lib/views/logInTemplate.js
+++ src/lib/views/logInTemplate.js
@@ -19,6 +19,7 @@ export const loginTemplate = () => {
             </div>
         </div>
     </div>
+    
   `;
   return loginHTML;
 };
--- src/lib/views/registerTemplate.js
+++ src/lib/views/registerTemplate.js
@@ -3,22 +3,26 @@ import { headerTemplate } from './headerTemplate.js';
 export const registerTemplate = () => {
   let registerHTML = headerTemplate();
   registerHTML += `
-    <main class="registerMain">
-      <text class= "registerTitle">
-        Regístrate para ingresar <br> a nuestra comunidad
-      </text>
+    <div id="register" class="loginModalRegister">
       <div class= "registerForm">
-          <h1> Nos alegra que quieras <br> crear junto a nosotros</h1>
-          <h2> Tenemos muchos proyectos <br> que te pueden gustar</h2>
-            <form id="registerForm">
-                <input type="text" id="registerEmail" class="inputForm" placeholder="Correo electrónico"/>
-                <input type="password" id="registerPassword" class="inputForm" placeholder="Ingresa una contraseña"/>
-                <button type="submit" class="createAccountButton" >Crear cuenta</button> 
-            </form>
-            <p class="option"> ó <p>  
-            <button id="googleRegisterButton" class="googleRegisterButton">Crear cuenta con Google</button>
+        <h1 class="h1Register"> Nos alegra que quieras <br> crear junto a nosotros</h1>
+          <text class= "registerTitle">
+            Regístrate
+          </text>
+        <h2 class="h2Register"> Tenemos muchos proyectos <br> que te pueden gustar</h2>
+                <form id="registerForm">
+                    <input type="text" id="registerEmail" class="inputForm" placeholder="Correo electrónico"/>
+                    <input type="password" id="registerPassword" class="inputForm" placeholder="Ingresa una contraseña"/>
+                    <button type="submit" class="createAccountButton" >Crear cuenta</button> 
+                </form>
+                <p class="option"> ó <p>  
+                <button id="googleRegisterButton" class="googleRegisterButton">Crear cuenta con Google</button>
+                <a href='' class='closeRegisterModal'>Cerrar</a>
+           </div>
       </div>
-    </main>`;
+    </div>
+  `;
+
   registerHTML += `
     <footer>
       <text class="doitHash">
