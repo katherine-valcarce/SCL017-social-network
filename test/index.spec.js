1e0ce4d6ee40901d5e5516de08fba4394906d982
ConsueloHerrera <consueloherrerabarguena@gmail.com>
2021-07-23 17:37:02 -0400

    test

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,8 +1,29 @@
 // importamos la funcion que vamos a testear
 import { displayRespectiveTemplate } from '../src/lib/router.js';
+import { googleRegister, register } from '../src/lib/index.js';
+import { registerTemplate } from '../src/lib/views/registerTemplate.js';
 
 describe('displayRespectiveTemplate', () => {
   it('should be a function', () => {
     expect(typeof displayRespectiveTemplate).toBe('function');
+  });
+});
+
+describe('googleRegister', () => {
+  it('should be a function', () => {
+    expect(typeof googleRegister).toBe('function');
+  });
+});
+
+describe('register', () => {
+  it('should be a function', () => {
+    expect(typeof register).toBe('function');
+  });
+});
+
+describe('containerRoot', () => {
+  it('should not return HTML', () => {
+    const registerHtml = registerTemplate();
+    expect(registerHtml instanceof HTMLElement).toBe(false);
   });
 });
