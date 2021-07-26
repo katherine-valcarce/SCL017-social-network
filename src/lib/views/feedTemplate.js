c262ba7d1ad1a01ed771e000ec5f2d055611fd7f
Katherine-valcarce <katherine.v@live.cl>
2021-07-26 09:08:43 -0400

    feed comentado

--- src/lib/views/feedTemplate.js
+++ src/lib/views/feedTemplate.js
@@ -1,14 +1,14 @@
 export const feedTemplate = () => {
   const feedHTML = `
-  <div class="grid-container">
-        <div class="categoriesMenu">categoriesMenu</div>
-        <div class="searcher">searcher</div>
-        <div class="menuBar">menuBar</div>  
-        <div class="createPost">createPost</div>
-        <div class="friendship-suggestions">friendship-suggestions</div>
-        <div class="Post">Post</div>
-  </div>
   
   `;
   return feedHTML;
 };
+/*  <div class="grid-container">
+        <div class="categoriesMenu">categoriesMenu</div>
+        <div class="searcher">searcher</div>
+        <div class="menuBar">menuBar</div>
+        <div class="createPost">createPost</div>
+        <div class="friendship-suggestions">friendship-suggestions</div>
+        <div class="Post">Post</div>
+  </div> */
